fa6cac48edf653af858400334d579ffd617fca1e
Jake <jke.srvices.accnts.anderson@gmail.com>
2026-03-25 15:32:55 +0200

    Add files via upload

---
+++ 2202373.js
@@ -0,0 +1,40 @@
+const reader = new Html5Qrcode("camera");
+let scannerOn = false; //don't use the interface to make decisions!!
+// Based on language needs (if needed to change language may become difficult and combersome)
+
+function toggleScanner() {
+    scannerOn = !scannerOn;
+    if (scannerOn) {
+        startScanner();
+        mapContainer.style.display = "none";
+        btn.innerText = "CANCEL";
+    } else{
+        stopScanner();
+        mapContainer.style.display = "block";
+        btn.innerText = "SCAN";
+    }
+}
+
+function startScanner() {
+    reader.start(
+        { facingMode: "environment" }, //User = selfy environment = back camera 
+        {}, // frame rate / specific things NOT NEEDED HERE
+        function (text)  {
+            const place = JSON.parse(text);
+            showMarkerAt(place.top, place.left);
+            toggleScanner();
+        }   
+    ).catch(function (err) {
+            console.error(err);
+        }); 
+}
+
+function stopScanner() {
+    reader.stop();
+}
+function showMarkerAt(top, left) {
+    marker.style.top = top;
+    marker.style.left = left; 
+}
+
+
